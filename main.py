8bc50169d4f67abebd648b72387d6cf4a44b0123
Ronald <ronaldagueropolanco@gmail.com>
2026-09-16 14:25:42 -0400

    Creamos la pantalla de la calculadora

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Mi Calculadora")
 ventana.geometry("300x400")
+
+# 2. CREAR LA PANTALLA (Entry)
+pantalla = tk.Entry(ventana, font=("Arial", 20), justify="right")
+pantalla.grid(row=0, column=0, columnspan=4, padx=10, pady=10) # Ocupa 4 columnas
